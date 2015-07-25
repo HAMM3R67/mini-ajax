58d4261208f872e9a3d90e83145111e7db5305b9
HAMM3R67 <tcschauer@gmail.com>
2015-07-25 10:31:48 -0600

    insertData  function complete

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-$('#getUSerrs').on('click', function(){
+$('#getUsers').on('click', function(){
 	return $.ajax({
 		method: 'GET',
 		url: 'http://reqr.es/api/users?page=1',
-		success: console.log("Yeah Baby!")
-	})
-})
+		success: function(res){
+			console.log(res);
+		}
+	});
+});
+
+var insertData = function(arr){
+	for (var i = 0; i < arr.length; i++){
+		$('#userInfo' + (i + 1)).html('<div>' +
+			'User Info:' +
+			'<li>' +
+			'First name: ' + arr[i].first_name +
+			'</li>' +
+			'<li>' +
+			'Last name: ' + arr[i].last_name +
+			'</li>' +
+			'<hr>' +
+			'</div>'
+			)
+	};
+}
